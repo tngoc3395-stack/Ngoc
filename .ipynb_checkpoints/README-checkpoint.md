f27a7103528cc9d6bd6e49f06c686e768b85d2ad
https://johntran038.github.io <243246271+tngoc3395-stack@users.noreply.github.com>
2026-07-18 19:11:54 +0000

    Pending changes exported from your codespace

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,7 @@
+# tngoc
+##Table of Contents
+-[Installation]_(#installation)
+-[Usage]_(#usage)
+-[Contributing]_(#contributing)
+-[License]_(#license)
+
---
+++ untitled.py
@@ -0,0 +1,24 @@
+python
+# Daily Glucose [
+   {
+    "date": "2026-06-15",
+    "glucose-mg-dL": 137,
+    "a1c-percentage": 7.6
+   },
+   {
+    "date": "2026-06-16",
+    "glucose-mg-dL": 152,
+    "a1c-percentage": 8.4
+   },
+   {
+    "date": "2026-06-17"
+    "glucose-mg-dL": 126,
+    "a1c-percentage": 7.0
+},
+{
+    "date": "2026-06-18"
+    "glucose-mg-dL": 133,
+    "a1c-percentage": 7.3,
+},
+{
+    
